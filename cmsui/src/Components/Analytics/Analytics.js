52348100d8b3a4c067c13d68d50fb638c0bd9bc4
Ravi Sharma <ravi.sharma26494@gmail.com>
2023-01-11 18:50:33 +0530

    Analytics page layout & deployment

--- cmsui/src/Components/Analytics/Analytics.js
+++ cmsui/src/Components/Analytics/Analytics.js
@@ -2,7 +2,22 @@ import React from 'react'
 
 const Analytics = () => {
   return (
-    <div>Analytics</div>
+    <div className='analytics'>
+      <div className='analytics__info'>
+        <div className='analytics__info--single'>
+          <div className='info__single--left'>
+            <p>48</p>
+            <p>Total Gym</p>
+          </div>
+        </div>
+        <div className='analytics__info--single'></div>
+        <div className='analytics__info--single'></div>
+      </div>
+      <div className='analytics__charts'>
+
+      </div>
+
+    </div>
   )
 }
 
